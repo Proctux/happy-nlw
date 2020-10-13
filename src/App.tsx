c0a3baf739502613c285fd7b1c728c84843705c2
Luiz <luiz.ado@hotmail.com>
2020-10-12 23:53:42 -0300

    Initializing the web project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+interface TitleProps {
+  text: string;
+}
+
+const Title = (props: TitleProps) => {
+  return <h1>{props.text}</h1>
+}
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title text="Título 1" />
+      <Title text="Título 2" />
+      <Title text="Título 3" />
+      <Title text="Título 4" />
     </div>
-  );
+  )
 }
 
 export default App;
